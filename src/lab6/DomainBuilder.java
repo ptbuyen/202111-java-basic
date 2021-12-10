7a02f5de789101eccbf8194d075a15ab8d520c89
ptbuyen <ptbuyen1995@gmail.com>
2021-12-10 19:52:25 +0700

    completed lab6

--- src/lab6/DomainBuilder.java
+++ src/lab6/DomainBuilder.java
@@ -8,7 +8,7 @@ public class DomainBuilder {
         String finalDomainType;
 
         urlPrefix = getUrlPrefix(sslType);
-        finalDomainType = getFinalDoaminType(domainType);
+        finalDomainType = getFinalDomainType(domainType);
 
         return urlPrefix.concat("://").concat(domainName).concat(finalDomainType);
     }
@@ -29,7 +29,7 @@ public class DomainBuilder {
         return  urlPrefix;
     }
 
-    private static String getFinalDoaminType(DomainType domainType) {
+    private static String getFinalDomainType(DomainType domainType) {
         String finalDomainType = "";
         switch (domainType){
             case NET:
